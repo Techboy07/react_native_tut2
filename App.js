ed338ac1fd25aa925479ae7cffa2ad44d492a05e
toluwalase <technogodwr@gmail.com>
2023-12-18 10:00:21 +0100

    activity bar

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  View,
+  Button,
+  StyleSheet,
+  Modal,
+  Text,
+  StatusBar,
+  ActivityIndicator,
+} from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <StatusBar
+        backgroundColor={"lightgreen"}
+        barStyle={"dark-content"}
+        hidden
+      />
+
+      <ActivityIndicator />
+      <ActivityIndicator
+        size={"large"}
+        color={"midnightblue"}
+        animating={false}
+      />
+      {/* <Button
+        title="Press"
+        onPress={() => {
+          setModalVisible(() => true);
+        }}
+        color="midnightblue"
+      />
+      <Modal
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+        animationType="slide"
+        presentationStyle="formSheet"
+      >
+        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
+          <Text>Modal content</Text>
+          <Button
+            title="Close"
+            color="midnightblue"
+            onPress={() => {
+              setModalVisible(() => false);
+            }}
+          />
+        </View>
+      </Modal> */}
     </View>
   );
 }
@@ -13,8 +57,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
